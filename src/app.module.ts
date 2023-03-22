94ff6e387542ca010d08efa5ae8049753822561f
Patryk Dulniok <patrykdulniok@op.pl>
2023-03-22 11:54:17 +0100

    Add configure to elephantsql

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,6 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,7 +6,6 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
